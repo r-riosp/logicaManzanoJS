eb990eb9152408b521aecadf0a8b74ead0a5c2bb
r-riosp <rafaelrios807@gmail.com>
2025-05-18 18:58:36 -0300

    Revert "feat: resolvido exercício L05K"
    This reverts commit 10f443378f4ff36b831473e24319e6b7f902b501.

--- manzano/L05/L05K/L05K.js
+++
@@ -1,7 +0,0 @@
-for (let contadora = 1; contadora < 10; contadora += 2) {
-    let fatorial = 1
-    for (let numero = contadora; numero >= 1; numero--) {
-        fatorial *= numero
-    }
-    console.log(`Fatorial de ${contadora} = ${fatorial}`)
-}
